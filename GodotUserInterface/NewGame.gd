48ceaf73c37d1033bd29c586be4bf76502451748
bakhtaward <78761693+bakhtaward@users.noreply.github.com>
2021-09-26 20:15:52 -0400

    start game

---
+++ GodotUserInterface/NewGame.gd
@@ -0,0 +1,8 @@
+extends Label
+
+func _ready():
+	pass # Replace with function body.
+
+func _on_NewGame_gui_input(event):
+	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
+		get_tree().change_scene("res://temp.tscn")
