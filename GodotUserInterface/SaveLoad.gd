da0689854e1ef7b765ef6f665cda8619139072c4
bakhtaward <78761693+bakhtaward@users.noreply.github.com>
2021-10-03 15:06:16 -0400

    Started working on save/load

--- GodotUserInterface/SaveLoad.gd
+++ GodotUserInterface/SaveLoad.gd
@@ -3,6 +3,6 @@ extends Label
 func _ready():
 	pass # Replace with function body.
 
-func _on_NewGame_gui_input(event):
+func _on_SaveLoad_gui_input(event):
 	if (event is InputEventMouseButton && event.pressed && event.button_index == 1):
-		get_tree().change_scene("res://NewGame.tscn")
+		get_tree().change_scene("res://SaveLoad.tscn")
