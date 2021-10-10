d175526b7ee229ef2aa98239660e470c7828f62b
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2021-10-10 15:09:43 -0400

    Game UI
    First work on game UI following flowcharts

---
+++ GodotUserInterface_Doug/rpgr_gameWork_20211010A/UserInterface/But_ChangeScene.gd
@@ -0,0 +1,12 @@
+#Tool lets you any code in editor, such as plugins
+tool
+extends Button
+
+#Creates param usable in the UI; and the params next to export make it string and file browser
+export(String, FILE) var next_scene_path: = ""
+
+func _on_But_NewGame_button_up():
+	get_tree().change_scene(next_scene_path)
+
+func _get_configuration_warning() -> String:
+		return "next_scene_path must be set for this button to work" if next_scene_path == "" else ""
