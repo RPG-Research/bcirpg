9828adff9e110a29560a6fff8dafb4a28c88413a
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2022-03-06 14:58:55 -0500

    Updates theme files to include more classes and styles
    Updates theme files for dark and light

--- Godot/MainMenu/GodotUserInterface_Doug/bcirpg_mainMenu_20220220A/screens/MenuScreen.gd
+++ Godot/MainMenu/GodotUserInterface_Doug/bcirpg_mainMenu_20220220A/screens/MenuScreen.gd
@@ -7,3 +7,6 @@ extends Control
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$VBoxContainer/But_NewGame.grab_focus()
+	#DKM TEMP: for testing only -- this will be set in settings
+	#theme=load("res://assets/ui_controlNode_light_theme.tres")
+	theme=load("res://assets/ui_controlNode_dark_theme.tres")
