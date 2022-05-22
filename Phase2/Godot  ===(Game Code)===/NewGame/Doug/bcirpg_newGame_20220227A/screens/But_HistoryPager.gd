52b3564519642e0a5905edb2f0ada2e82c59a0e6
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2022-05-22 15:19:44 -0400

    History Array Pager Display
    This update displays the history from singleton history array.

--- Phase2/Godot  ===(Game Code)===/NewGame/Doug/bcirpg_newGame_20220227A/screens/But_HistoryPager.gd
+++ Phase2/Godot  ===(Game Code)===/NewGame/Doug/bcirpg_newGame_20220227A/screens/But_HistoryPager.gd
@@ -1,5 +1,5 @@
 #BUT_HISTORYPAGER:
-# Simply moves through array page -- should call the HistoryViewer script to display 
+# Iterates the page number and calls HistoryViewer script to display 
 # 	stored page and response fro the history array
 #
 
@@ -7,11 +7,7 @@ extends Button
 
 onready var historyViewerScript = get_node("/root/HistoryViewer")
 
-
-
+#DKM TEMP: this needs refactoring -- too much being calculated as needed/repeated
 func _on_But_HistoryPager_button_up() -> void:
-	if(historyViewerScript.current_history_page_no < historyViewerScript.history_source.output_history_array.size()):
-		historyViewerScript.current_history_page_no = historyViewerScript.current_history_page_no +1
-	else:
-		historyViewerScript.current_history_page_no = 0	
-	print("Current page: " + str(historyViewerScript.current_history_page_no))
+	historyViewerScript.update_pager()
+	
--- Phase2/Godot  ===(Game Code)===/NewGame/Doug/bcirpg_newGame_20220227A/screens/HistoryViewer.gd
+++ Phase2/Godot  ===(Game Code)===/NewGame/Doug/bcirpg_newGame_20220227A/screens/HistoryViewer.gd
@@ -8,7 +8,9 @@ extends Control
 
 onready var history_source = get_node("/root/History").historyScreensSingleton
 onready var current_history = $Background/MarginContainer/Rows/GameInfo/CurrentHistory
+#For zero-indexed array position:
 onready var current_history_page_no = 0
+onready var page_displayer = $Background/MarginContainer/Rows/ItemList/Page_Display
 
 #Abstract class we instance when wanted in game as child of HistoryReference
 const TextOutput = preload("res://UserInterface/Response.tscn")
@@ -18,23 +20,30 @@ const InputResponse = preload("res://UserInterface/InputResponse.tscn")
 func _ready() -> void:
 	#DKM TEMP: for testing only -- this will be set in settings
 	theme=load("res://assets/ui_controlNode_dark_theme.tres")
-	#DKM_TEMP: display size of current history:
+
 	print("Loaded history array size is: " + str(history_source.output_history_array.size()))
-
-
-#Handles input text
-func create_response(response_text: String):
-	var response = TextOutput.instance()
-	response.text = response_text
-	add_response_to_game(response)	
+	update_pager()
+	#DKM TEMP:
+#	page_displayer.text = "1"
+#	if(current_history.get_child_count() > 0):
+#		current_history.remove_child(current_history.get_child(0))
+#	var opening_res = history_source.output_history_array[0]
+#	current_history.add_child(opening_res)
 
 #Copies the response output to add to both current game output, and the 
 #	history array. 
-func add_response_to_game(response: Control):
+func add_display_to_game(response: Control):
 	if(current_history.get_child_count() > 0):
 		current_history.remove_child(current_history.get_child(0))
 	current_history.add_child(response)
-
+	
+func update_pager():
+	if(current_history_page_no >= history_source.output_history_array.size()):
+		current_history_page_no = 0	
+	print("Current page: " + str(current_history_page_no))
+	add_display_to_game(history_source.output_history_array[current_history_page_no])
+	current_history_page_no = current_history_page_no +1
+	page_displayer.text = str(current_history_page_no) 
 
 
 
