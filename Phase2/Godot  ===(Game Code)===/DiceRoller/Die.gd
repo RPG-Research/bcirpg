3a4ca5fdcdc668f733873c97064264a652aa21ef
Andrew Tang <tandrew@gmail.com>
2022-07-11 01:39:53 +0800

    Squashed commit of the following:
    commit 59b74b946a090dd0b95e21814fe2ed7751b6e09b
    Author: Andrew Tang <tandrew@gmail.com>
    Date:   Mon Jul 11 01:16:34 2022 +0800
    
        Complete DiceRoller Singleton Version
    
        Created script: "DiceRoller.gd" to be the only dice script attached to a node and will act as a singleton. A single DieManager object is created in this script. The DieManager's values can be changed and methods can be called through the DiceRoller Script
    
    commit 3a402d43316599c1a883ab583583942dd7723d7b
    Author: Andrew Tang <90939405+TangoDevelopments@users.noreply.github.com>
    Date:   Sun Jul 3 13:05:28 2022 -0400
    
        Delete .DS_Store
    
    commit c00b24c60ac3cc9a39a93b79cf3352241b1c9186
    Author: Andrew Tang <90939405+TangoDevelopments@users.noreply.github.com>
    Date:   Sun Jul 3 13:05:16 2022 -0400
    
        Delete .DS_Store
    
    commit 4f788de0c7676fec60624f50be6284b4f2e419ac
    Author: Andrew Tang <90939405+TangoDevelopments@users.noreply.github.com>
    Date:   Sun Jul 3 13:05:05 2022 -0400
    
        Delete .DS_Store
    
    commit c20fd855676052b135e7c99af9800af86ad81060
    Author: Andrew Tang <tandrew@gmail.com>
    Date:   Mon Jul 4 00:13:15 2022 +0800
    
        Added D10 and easier die selection

--- Phase2/Godot  ===(Game Code)===/DiceRoller/Die.gd
+++ Phase2/Godot  ===(Game Code)===/DiceRoller/Die.gd
@@ -1,17 +1,10 @@
 #Die class
-
-extends Button
+extends Node2D
 
 class_name Die
 
-#array of valid number of faces
-var dieTypes = [4, 6, 8, 10, 12, 20]
-
 #value of selected die type
 var numFaces: int
-
-#bool will be used to check if a valid die type was chosen
-var validNumFaces = false
 
 #Class constructor
 func _init(value):
@@ -21,18 +14,11 @@ func _init(value):
 func rollDie():
 	randomize()
 	
-	#check if selected die type exists in dieTypes array
-	for value in dieTypes:
-		if value == numFaces:
-			validNumFaces = true
-	
-	#if the selected die type exists, we can roll an integer value based on the number of sides.
-	if validNumFaces:
-		var rolledNum
-		rolledNum = randi() % numFaces + 1
-		return rolledNum
-	else:
-		print("You do not have a valid number of sides on the die!")
-		return
+	var rolledNum
+	rolledNum = randi() % numFaces + 1
+	return rolledNum
 		
 		
+#Returns the number of faces on this die
+func getNumFaces():
+	return numFaces
