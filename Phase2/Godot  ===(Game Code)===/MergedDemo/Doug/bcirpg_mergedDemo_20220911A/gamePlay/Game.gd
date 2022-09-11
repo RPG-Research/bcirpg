2f51e2228aee2ee2b4390ec55ba7053f14bd5b9f
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2022-09-11 16:34:30 -0400

    Continued module updates:
    The following changes are included in this update:
    	-TestingFiles subdirectory replaced with the _userFiles subdirectory. As discussed in meeting, this is to house what's been previously moved to OS-dependent user directory.
    	-user paths are removed for a the res://_userFiles location throughout.
    	-options are now being both dynamically loaded and linking from the JSON.
    	-The test JSON adds several (silly) options to test having more than 3 and larger test outputs.
    	-Attempted to incorporate Luke's updates from today in this update.
    	-NOTE: cleanup/refactor is definitely needed, but got this working today. The CommandProcessor, for example can likely be removed in the next update

---
+++ Phase2/Godot  ===(Game Code)===/MergedDemo/Doug/bcirpg_mergedDemo_20220911A/gamePlay/Game.gd
@@ -0,0 +1,170 @@
+#GAME: 
+# Controls output space, loading descriptions and options for each locale and 
+#	appending to the history array.
+
+extends Control
+
+#Source for module file: to the GU
+#export(String, FILE, "*.json") var module_file_path:String
+#DKM TEMP:
+var module_file_path = "res://_userFiles/Module_Demo_001.json"
+
+onready var history_source = get_node("/root/History").historyScreensSingleton
+onready var settings = get_node("/root/GlobalSaveInstance").settingsInstance
+
+#Abstract class we instance when wanted in game as child of HistoryReference
+const TextOutput = preload("res://UserInterface/Response.tscn")
+const InputResponse = preload("res://UserInterface/InputResponse.tscn")
+const OptionOutput = preload("res://UserInterface/Option.tscn")
+
+onready var command_processor = $CommandProcessor
+onready var current_text = $Background/MarginContainer/Rows/GameInfo/CurrentText
+onready var history_pager = $Background/MarginContainer/Rows/ItemList/But_History_Page
+onready var options_container = $Background/MarginContainer/Rows/InputArea/OptionsContainer
+onready var option_one = $Background/MarginContainer/Rows/InputArea/VBoxContainer/option1
+onready var option_two= $Background/MarginContainer/Rows/InputArea/VBoxContainer/option2
+onready var option_three = $Background/MarginContainer/Rows/InputArea/VBoxContainer/option3
+onready var pSingleton = get_node("/root/PlayerCharacter")
+onready var charSheet = $Con_charSheet/MarginContainer/VBoxContainer/CharacterSheet
+
+
+#DKM TEMP: this is just a temp file solution for grabbing map/module, will be replaced with DB
+#	or desired load approach
+onready var module_map = "res://_userFiles/temp_map.save"
+
+var nodeArray
+
+func _ready() -> void:
+	save_module()
+	theme=load(settings.themeFile)
+	
+	#DKM TEMP: testing:
+	var moduleDict = loadJSONToDict(module_file_path)
+	
+	#DKM TEMP: this needs to be refactored to reposition this initialization
+	var i = 0
+	print("Looking for JSON at: " + module_file_path)
+	print("Json returned as: " + str(moduleDict))
+	var initialNode = Locale.new()
+	nodeArray = [initialNode]
+	for moduleNode in moduleDict.values():
+		if nodeArray.size() <= i:
+			var newNode = Locale.new()
+			nodeArray.append(newNode)
+		nodeArray[i].locale_name = moduleNode.get("Id")
+		nodeArray[i].locale_description = moduleNode.get("Text")
+		var nodeOptions = moduleNode.get("Option_Labels")
+		for option in nodeOptions:
+			nodeArray[i].options_array.append(option)
+			print("For #" + str(i) + ": appended option of: " + str(option))
+		var nodeDestinations = moduleNode.get("Option_GoTos")
+		for dest in nodeDestinations:
+			nodeArray[i].destinations_array.append(dest)
+			print("For #" + str(i) + ": appended go to destination of: " + str(dest))
+		print("Node array name for #" + str(i) + ": " + nodeArray[i].locale_name)
+		i = i+1
+	
+	current_text.show()
+	
+	#Load character sheet:
+	charSheet.text = pSingleton.pc.pcText
+	
+	create_response(nodeArray[0].locale_description)
+	#DKM TEMP: another that needs to be broken out when ready:
+	clear_prior_options()
+	i = 0
+	for option in nodeArray[0].options_array:
+		var destArr = nodeArray[0].destinations_array
+		create_option(option, destArr[i])
+		i = i+1
+
+#DKM TEMP: convert JSON file to dictionary for module import:
+func loadJSONToDict(filepath:String)->Dictionary:
+	var file = File.new()
+	#assert file.file_exists(filepath)
+	file.open(filepath,file.READ)
+	var moduleDict = parse_json(file.get_as_text())
+	#assert moduleDict.size() > 0
+	return moduleDict
+
+
+
+#Below temporarily takes user selection and appends it to responses; adding new instances 
+#	of our input row with an input and response pair for each
+func handleUserInput(user_choice: String) -> void:
+	var input_response = InputResponse.instance()
+	var inputText = "User selected: " + user_choice
+	var response = command_processor.process_command(user_choice)
+	input_response.set_text(inputText, response)
+	add_response_to_game(input_response)
+	
+#Handles input text
+func create_response(response_text: String):
+	var response = TextOutput.instance()
+	response.text = response_text
+	add_response_to_game(response)	
+
+#Copies the response output to add to both current game output, and the 
+#	history array. 
+func add_response_to_game(response: Control):
+	add_response_to_history(response)
+	if(current_text.get_child_count() > 0):
+		current_text.remove_child(current_text.get_child(0))
+	current_text.add_child(response)
+
+func add_response_to_history(response: Control) -> void:
+	#DKM TEMP: so here we
+	#1. var response_history = response.duplicate()
+	var response_for_history = response.duplicate()
+	#2. get the history array from the singleton,
+	#3. Add this to the history array
+	history_source.output_history_array.append(response_for_history)
+
+func clear_prior_options() -> void:
+	for n in options_container.get_children():
+		options_container.remove_child(n)
+		n.queue_free()
+
+func create_option(option: String, dest: String) -> void:
+	print("Received request to make option for label: " + option +"; and destination: " + dest)
+	var optionNew = OptionOutput.instance()
+	optionNew.destinationLabel = dest
+	optionNew.text = option
+	add_option_to_game(optionNew)
+
+func add_option_to_game(optionNew: Control) -> void:
+	options_container.add_child(optionNew)
+	var newOptNumber = options_container.get_child_count()
+	if newOptNumber-1 >= 0:
+		print("New added opt is: " + str(newOptNumber-1))
+		options_container.get_child(newOptNumber-1).connect("option_pressed", self, "_on_option_pressed")
+
+#DKM TEMP: these need to be dynamically added with the options themselves
+
+func _on_option_pressed(destinationSelected: String) -> void:
+	#print("Destination node for pressed option is: " + destinationSelected)
+	change_node(destinationSelected)
+	
+func get_node_by_name(nodeName: String) -> Locale:
+	for n in nodeArray:
+		if n.locale_name == nodeName:
+			return n
+	return nodeArray[0]
+
+func change_node(destinationNode: String) -> void:
+	var target_Locale = get_node_by_name(destinationNode)
+	create_response(target_Locale.locale_description)
+	#DKM TEMP: another that needs to be broken out when ready:
+	clear_prior_options()
+	var i = 0
+	for option in target_Locale.options_array:
+		var destArr = target_Locale.destinations_array
+		create_option(option, destArr[i])
+		i = i+1
+
+
+#DKM TEMP: saves the entire scene in one packed scene file
+func save_module():
+		var scene = PackedScene.new()
+		scene.pack(self)
+		ResourceSaver.save("user://game_01.tscn", scene)
