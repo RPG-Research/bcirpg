1b55da71e92a0873c380ef57cebb9b85c8e9638b
bakhtaward <78761693+bakhtaward@users.noreply.github.com>
2022-09-25 11:53:39 -0400

    Import character from a little while ago (only works with .txt files)

--- Phase2/Godot  ===(Game Code)===/Import Character/screens/grabFocus.gd
+++ Phase2/Godot  ===(Game Code)===/Import Character/screens/grabFocus.gd
@@ -1,35 +1,60 @@
 extends Control
-var charFilePath
-var a
-#GRAB FOCUS: simple script for temp files to grab focus
-
+#charFilePath will hold the file path for the character file, fileText will hold the text in the file
+var charFilePath = ""
+var fileText
 
 func _ready() -> void:
+	#grab focus will make it so tab can be used to move between options
 	$VBoxContainer2/Title/But_ChangeScene.grab_focus()
+	
+	var save_game = File.new()
+	if save_game.file_exists("user://savegame.save"):
+		save_game.open("user://savegame.save", File.READ)
+		var tempPath = save_game.get_as_text()
+		displayInfo(tempPath)
+	
+	save_game.close()
 
+func displayInfo(path):
+	var file = File.new()
+	#read the text in the file, save it in the variable fileText, display in textBox
+	file.open(path, File.READ)
+	
+	fileText = file.get_as_text()
+	
+	$VBoxContainer2/VBoxContainer/VBoxContainer3/textBox.visible = true
+	$VBoxContainer2/VBoxContainer/VBoxContainer3/textBox.text = fileText
+#	
+	
+	#save the file path in the global var to use it in the other function
+	charFilePath = path
+	file.close()
+	
 
 func _on_Button_pressed():
 	$VBoxContainer2/VBoxContainer/FileDialog.popup()
 
-#this is going to take information from the file the player chose and put the individul parts into textboxes so it can be edited and then saved
 func _on_FileDialog_file_selected(path):
+	displayInfo(path)
+	
+#when they've made the changes and pressed save:
+func _on_Save_Button_pressed():
+	#open the same file so that changes can be written to it
 	var file = File.new()
-	#read the text in the file, save it in the variable a
-	file.open(path, File.READ)
-#	a = int(file.get_line())
-	a = file.get_as_text()
-	#split by the spaces so the individual pieces can be separated into textboxes
-	var b = a.split(" ")
-#	for i in b.size():
-	var i = 0
-	while i < b.size():	
-		#make a new textbox for each piece of information
-		var textLine = Label.new()
-		$VBoxContainer2/VBoxContainer.add_child(textLine)
-		textLine.text = b[i]
-		i += 1
-		var textBox = LineEdit.new()
-		$VBoxContainer2/VBoxContainer.add_child(textBox)
-		textBox.text = b[i]
-		i += 1
+	file.open(charFilePath, File.WRITE)
+	
+	#store the text from the textbox back into the file
+	var content = ""
+	var currentWord = $VBoxContainer2/VBoxContainer/VBoxContainer3/textBox.text
+		
+	content = currentWord
+	file.store_string(content)
 	file.close()
+
+
+func _on_But_Remember_pressed():
+	#save the path of the file so that it can be automatically opened next time
+	var save_game = File.new()
+	save_game.open("user://savegame.save", File.WRITE)
+	save_game.store_string(charFilePath)
+	save_game.close()
