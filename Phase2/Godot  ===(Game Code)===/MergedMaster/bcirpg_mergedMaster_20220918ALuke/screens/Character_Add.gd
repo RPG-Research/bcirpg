733195593d3faefa1e860cbc813a11750c60cc92
PersonGuyGit <lukegebbink@gmail.com>
2022-10-02 12:51:55 -0600

    Added some changes to the CSV system for Add Character

--- Phase2/Godot  ===(Game Code)===/MergedMaster/bcirpg_mergedMaster_20220918ALuke/screens/Character_Add.gd
+++ Phase2/Godot  ===(Game Code)===/MergedMaster/bcirpg_mergedMaster_20220918ALuke/screens/Character_Add.gd
@@ -3,31 +3,51 @@
 #		it into the character object
 extends Control
 
+var originalPath = "res://_userFiles/Template.csv" 
+var rows
+
 onready var settings = get_node("/root/GlobalSaveInstance").settingsInstance
 
-
+onready var SaveButton = get_node("HBoxContainer/VBoxContainer/SaveButton")
 
 onready var nameVar = get_node("HBoxContainer/VBoxContainer/LabelName/LineEditName")
-onready var profVar = get_node("HBoxContainer/VBoxContainer/LabelProf/LineEditProf")
+onready var profVar = get_node("HBoxContainer/VBoxContainer/LabelProfession/LineEditProf")
 onready var strengthVar = get_node("HBoxContainer/VBoxContainer/LabelStrength/LineEditStrength")
 onready var intelVar = get_node("HBoxContainer/VBoxContainer/LabelIntellect/LineEditIntel")
 onready var willpowerVar = get_node("HBoxContainer/VBoxContainer2/LabelWillpower/LineEditWill")
 onready var charmVar = get_node("HBoxContainer/VBoxContainer2/LabelCharm/LineEditCharm")
 onready var weaponVar = get_node("HBoxContainer/VBoxContainer2/LabelWeapon/LineEditWeapon")
 onready var armorVar = get_node("HBoxContainer/VBoxContainer2/LabelArmor/LineEditArmor")
-onready var quoteVar = get_node("HBoxContainer/VBoxContainer/LabelQuote/LineEditQuote")
+onready var quoteVar = get_node("HBoxContainer/VBoxContainer2/LabelQuote/LineEditQuote")
 
 func writeToCSV(nameV, profV, strengthV, intelV, willpowerV, charmV, weaponV, armorV, quoteV):
-	pass
-
-
+	var f = File.new()
+	f.open("res://_userFiles/CharacterOutput.csv", File.WRITE)
+	var ArrayStrings = [PoolStringArray()]
+	
+	f.store_csv_line(["Name", nameV])
+	f.store_csv_line(["Profession", profV])
+	f.store_csv_line(["Strength", strengthV])
+	f.store_csv_line(["Intellegence", intelV])
+	f.store_csv_line(["Willpower", willpowerV])
+	f.store_csv_line(["Charm", charmV])
+	f.store_csv_line(["Weapon", weaponV])
+	f.store_csv_line(["Armor", armorV])
+	f.store_csv_line(["Quote", quoteV])
+	f.close()
+	
+	
+func _process(delta):
+	if SaveButton.pressed == true:
+		$Title/FileDialog.popup()
+				
+		writeToCSV(nameVar.get_text(), profVar.get_text(), 
+		strengthVar.get_text(), intelVar.get_text(), willpowerVar.get_text(), 
+		charmVar.get_text(), weaponVar.get_text(), armorVar.get_text(), quoteVar.get_text())
+		
 func _ready() -> void:
 	theme=load(settings.themeFile)
-	$Title/But_SaveChar.grab_focus()
-
-func _on_But_SaveChar_pressed() -> void:
-	$Title/FileDialog.popup()
-
+	
 #DKM TEMP: just text for now from text edit
 func _on_FileDialog_file_selected(path: String) -> void:
 	var pc = get_node("/root/PlayerCharacter")
