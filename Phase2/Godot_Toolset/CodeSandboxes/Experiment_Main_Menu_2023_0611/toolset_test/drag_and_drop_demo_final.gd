db553dea28026239f9a9226875313a31703ed141
PersonGuyGit <lukegebbink@gmail.com>
2023-06-11 12:36:40 -0600

    Forked the project that Doug made; so I could make an Index home page for the toolset.

---
+++ Phase2/Godot_Toolset/CodeSandboxes/Experiment_Main_Menu_2023_0611/toolset_test/drag_and_drop_demo_final.gd
@@ -0,0 +1,9 @@
+extends PanelContainer
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	$VLayout/DragAndDropColumns/SourceContainer/FileDialog.popup()
+
+
+
---
+++ Phase2/Godot_Toolset/CodeSandboxes/Experiment_Main_Menu_2023_0611/toolset_test_tabbed/Control_Manual_MasterScript.gd
@@ -0,0 +1,16 @@
+extends PanelContainer
+
+
+# Declare member variables here. Examples:
+# var a: int = 2
+# var b: String = "text"
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	var outputFolderStr = "Region\n\tLocation\n\t\tSpace\n\t\t\tScene" + "\n\t\tSpace"
+	$VLayout/DragAndDropColumns/SourceContainer/TextEdit.text = outputFolderStr
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta: float) -> void:
+#	pass
