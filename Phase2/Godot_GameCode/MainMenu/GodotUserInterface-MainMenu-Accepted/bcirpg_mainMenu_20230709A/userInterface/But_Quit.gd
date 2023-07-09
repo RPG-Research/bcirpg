133e4ac53afdd6b7e4fb0347cbcde2f0a332fae7
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2023-07-09 16:14:56 -0400

    Main Menu script attachment fix
    This posts a modification of the Main Menu Godot project that addresses incorrect script links (due to Windows/MacOS case insensitivity).

---
+++ Phase2/Godot_GameCode/MainMenu/GodotUserInterface-MainMenu-Accepted/bcirpg_mainMenu_20230709A/userInterface/But_Quit.gd
@@ -0,0 +1,34 @@
+#BUT_QUIT:
+# Unique script for ending a game. Will additionally perform or call needed 
+#	shut down and close-out functionality.
+
+extends Button
+
+var history_JSON = "user://history.json"
+
+
+#DKM TEMP: save history is only here temporarily -- needs to move upstream.
+func _on_But_Quit_button_up():
+	get_tree().quit()
+	
+	
+#JSON: requires dictionaries:
+#func _saveHistory_JSON() -> void:
+#	var history_screens_arr = get_node("/root/History").historyScreensSingleton.output_history_array
+#	var file = File.new()
+#	file.open(history_JSON, File.WRITE)
+#	file.store_string(to_json(history_screens_arr))
+#
+#	file.close()
+#	#	#DKM TEMP: 
+#	print("Saved history array size should be: " + str(history_screens_arr.size()))
+	
+	
+#DKM TEMP:
+#tres file:
+#func _saveHistory() -> void:
+#	var history_screens = get_node("/root/History").historyScreensSingleton
+#	assert(ResourceSaver.save("user://history.tres", history_screens)==OK)
+#	#DKM TEMP: 
+#	print("Saved history array size: " + str(history_screens.output_history_array.size()))
+	
