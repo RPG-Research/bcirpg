81ed1d7c0385aa1a8ea6713e22d746cf479853ac
BassS0ul <73126509+BassS0ul@users.noreply.github.com>
2023-10-08 12:27:00 -0500

    lots of stuff added to menu, been forgetting to push

--- Phase2/Godot_Toolset/CodeSandboxes/Experiment_Main_Menu_2023_0611/GenreSettings.gd
+++ Phase2/Godot_Toolset/CodeSandboxes/Experiment_Main_Menu_2023_0611/GenreSettings.gd
@@ -18,5 +18,13 @@ func _on_new_genre_button_up():
 	$HBoxContainer/Default/ScrollContainer/GenreContainer.move_child($HBoxContainer/Default/ScrollContainer/GenreContainer/NewGenre, $HBoxContainer/Default/ScrollContainer/GenreContainer.get_child_count() - 1)
 
 
-func _on_v_scroll_bar_scrolling():
-	$HboxContainer
+
+
+func _on_save_button_pressed():
+	var file = FileAccess.open("user://test.xml", FileAccess.WRITE)
+	if file:
+		file.store_string("<?xml version=\"1.0\" encoding=\"UTF-8\"?>\n<genresettings>\n")
+		#for each genre container
+		#add name and what it's based on. skip based on if first three.
+		file.store_string("</genresettings>")
+		file.close()
