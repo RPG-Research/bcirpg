585e5bfc0e593e0c1341f33bd2fab49ff97c7079
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2023-10-17 09:50:30 -0400

    PostgreSQL Database Test Code for Godot
    Adds simple test Godot program for database access. (This specific to PostgreSQL, but can be adapted.) Sourced from our menu and Luke's provided reference: https://godotengine.org/asset-library/asset/985
    
    Note: before using, update the screens/DBTest_Postgres.gd script for relevant testing user, password, host, port, database values. (Search on comment "TODO." For anything aside from this rudimentary testing we obviously cannot write these values directly in the code.)
    
    Any SQL tests you need to run can be updated in the _executer function code block. Currently it expects a characters database that it just selects all and prints to console for verification testing.

---
+++ Phase2/Databases/Sandbox_Test_Godot_Code/Doug/Database_Testing_2023_1001A/screens/DBTest_Postgres.gd
@@ -0,0 +1,82 @@
+extends Node
+
+var database := PostgreSQLClient.new()
+
+#TODO: ADD RELEVANT VALUES BELOW FOR TESTING!
+const USER = ''#"postgres"
+const PASSWORD = ''
+const HOST = ''#"localhost"
+const PORT = 5432 # Default postgres port
+const DATABASE = ''#"postgres"
+
+
+
+func _ready() -> void:
+	print("Test is running")
+
+func _init() -> void:
+	print("Init running")
+	var _error := database.connect("connection_established", self, "_executer")
+	_error = database.connect("authentication_error", self, "_authentication_error")
+	_error = database.connect("connection_closed", self, "_close")
+	
+	#Connection to the database
+	_error = database.connect_to_host("postgresql://%s:%s@%s:%d/%s" % [USER, PASSWORD, HOST, PORT, DATABASE])
+
+
+func _physics_process(_delta: float) -> void:
+	print("Physics running")
+	database.poll()
+
+
+func _authentication_error(error_object: Dictionary) -> void:
+	prints("Error connection to database:", error_object["message"])
+
+
+func _executer() -> void:
+	print("Executor running")
+	print(database.parameter_status)
+	
+#	var datas := database.execute("""
+#		BEGIN;
+#		/*Helloworld*/
+#		SELECT concat('Hello', 'World');
+#		COMMIT;
+#	""")
+
+	var datas := database.execute("""
+		BEGIN;
+		SELECT * FROM characters;
+		COMMIT;
+	""")
+	
+	
+	
+	#The datas variable contains an array of PostgreSQLQueryResult object.
+	for data in datas:
+		#Specifies the number of fields in a row (can be zero).
+		print(data.number_of_fields_in_a_row)
+
+		# This is usually a single word that identifies which SQL command was completed.
+		# note: the "BEGIN" and "COMMIT" commands return empty values
+		print(data.command_tag)
+
+		print(data.row_description)
+
+		print(data.data_row)
+			
+		prints("Notice:", data.notice)
+	
+	if not database.error_object.empty():
+		prints("Error:", database.error_object)
+
+	database.close()
+
+
+func _close(clean_closure := true) -> void:
+	prints("DB CLOSE,", "Clean closure:", clean_closure)
+
+
+func _exit_tree() -> void:
+	print("Exit running")
+	database.close()
