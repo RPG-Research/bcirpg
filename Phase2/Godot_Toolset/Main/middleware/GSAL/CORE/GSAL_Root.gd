2803108e0a0d519025b371666faca766472e4f24
PersonGuyGit <lukegebbink@gmail.com>
2024-01-07 14:06:59 -0700

    Refactored folder structure for GSAL prep

---
+++ Phase2/Godot_Toolset/Main/middleware/GSAL/CORE/GSAL_Root.gd
@@ -0,0 +1,27 @@
+extends Node
+
+
+func _ready():
+	pass
+
+
+# Things to add to the GSAL
+
+#Work of a precentage for the character sheets and the rules. Use the range like 1-10 or 1-100
+
+##What do these two variables do? That was not clearly defined in the UML...
+#var Action_Declared : Array
+#
+#var Situation_Mod : int
+#
+## What is the desired data that we want from these two Int values below
+#var Defender_Results_Calculation : int
+#
+#var Opponent_Results_Calclation: int
+
+#func _Defender_VS_Opponent_Results(int): Matrix
+#Matrix is not a built in datatype in Godot. How I am imagining us doing this is with a 2D or 3D array.
+#Example Structure [[DefenderResults], [OpponentResults]]
+
+
+#func _Outcome_Options(Matrix): List
