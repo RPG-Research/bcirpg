2b9e6b1120b377c3237a3b2728a57de19726401b
MacDugRPG <57243055+MacDugRPG@users.noreply.github.com>
2024-02-04 14:57:34 -0500

    MVP Game Code (Initial)
    Adds initial MVP game code from the merged demo web version. See project README for more.
    
    Intro notes:
    	This project updates the RPGR BCI merged demo game from August 6, 2023 for
    	the Modgodtoolset and associated updates. Draws game from XML, and adds
    	numerous missing features.
    
    	The roadmap for this file can be found in the project Github:
    		Phase2/Documentation

---
+++ Phase2/Godot_GameCode/_MVP_Game_Demo/Doug/bcirpg_game_mvp_2024_0204A/globalScripts/globalSaveInstance.gd
@@ -0,0 +1,44 @@
+extends Node
+
+# Declare new settings template object
+var settingsInstance =  PlayerSettingsTemplate.new()
+var settings_file = "res://_userFiles/PlayerPreferences.cfg"
+
+func _ready() -> void:
+	load_settings_file() 
+	
+	#Config/ini:
+func load_settings_file():
+	var config = ConfigFile.new()
+	# Load data from a file.
+	var err = config.load(settings_file)
+	# If the file didn't load, ignore it.
+	if err != OK:
+		return
+	#Preferences:
+	settingsInstance.inputName = config.get_value("player_preferences", "player_name")
+	settingsInstance.riskFactor = config.get_value("player_preferences", "risk_threshold")		
+	#Controls:
+	settingsInstance.fontSize = config.get_value("visual_controls", "font_size")
+	settingsInstance.brightness = config.get_value("visual_controls", "brightness")	
+	#General Settings:
+	settingsInstance.volume = config.get_value("general_settings", "volume")
+	settingsInstance.bClosedCaptions = config.get_value("general_settings", "closed_captions")	
+	settingsInstance.bdevConsole = config.get_value("general_settings", "dev_console")				
+	#Keyboard:
+	settingsInstance.visualKeyboardLayout = config.get_value("virtual_keyboard", "keyboard_layout")	
+	#Theme:
+	settingsInstance.themeChoiceInt = config.get_value("theme", "theme_selection")	
+	
+	load_themeFile()
+	
+	#DKM TEMP: working
+	print("Input name loaded as: " + str(settingsInstance.inputName))
+	print("Theme loaded as: " + str(settingsInstance.themeFile))
+	
+func load_themeFile() -> void:
+	#DKM TEMP: this shouldn't be hard-coded:
+	if(settingsInstance.themeChoiceInt == 1):
+		settingsInstance.themeFile = "res://assets/ui_controlNode_light_theme.tres"
+	else:
+		settingsInstance.themeFile = "res://assets/ui_controlNode_dark_theme.tres"
